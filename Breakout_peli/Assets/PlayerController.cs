41ff8c941ee20890e30f1e1abda7197d46265152
kieti-bc <tia.kiesilainen@bc.fi>
2024-10-22 15:52:26 +0300

    Simplified player movement

--- Breakout_peli/Assets/PlayerController.cs
+++ Breakout_peli/Assets/PlayerController.cs
@@ -10,11 +10,7 @@ public class PlayerController : MonoBehaviour
     public float Slowdown = 800.0f;
     public float MaxVelocity = 100.0f;
     public float StopTreshold = 0.1f;
-
-    public float DynamicMultiplier = 1.0f;
-    public float DynamicSlowDown = 1.3f;
-
-    bool Dynamic = true;
+    public float speed = 1.0f;
 
     private float velocity = 0.0f;
     private Rigidbody2D rb;
@@ -24,7 +20,19 @@ public class PlayerController : MonoBehaviour
        rb = GetComponent<Rigidbody2D>(); 
     }
 
+	// TODO: How to stop player at wall?
+
 	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        // stop when hits wall
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("Player hit wall");
+            velocity = 0.0f;
+        }
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
 	{
         // stop when hits wall
         if (collision.gameObject.tag == "Wall")
@@ -46,88 +54,70 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+	private void Update()
+	{
+		AcceleratingMove();
+	}
+
+    void SimpleMove()
     {
-        // Kinematic physics 
-        // Velocity = CurrentVelocity + Acceleration
-        // Pos = currentPos + Velocity
-		float dt = Time.fixedDeltaTime;
+		Vector2 move = Vector2.zero;
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			move += new Vector2(-1.0f, 0.0f) * speed * Time.deltaTime;
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			move += new Vector2(+1.0f, 0.0f) * speed * Time.deltaTime;
+		}
+		rb.MovePosition(rb.position + move);
+	}
 
-        if (rb.bodyType == RigidbodyType2D.Kinematic)
-        {
-            float horizontal = Input.GetAxis("Horizontal") * Acceleration * dt;
-            if (horizontal == 0.0f && Mathf.Abs(velocity) > StopTreshold)
-            {
-                // No input, slow down
-                float slow = Slowdown;
-                float sign = GetSign(velocity);
-                // If going right, slow down is negative
-                if (velocity > 0.0f)
-                {
-                    slow = -Slowdown;
-                }
-                velocity += slow * dt;
+	void AcceleratingMove()
+	{
+        float horizontal = Input.GetAxis("Horizontal");
+		if (horizontal == 0.0f && Mathf.Abs(velocity) > StopTreshold)
+		{
+			// No input, slow down
+			float slow = Slowdown;
+			float sign = GetSign(velocity);
+			// If going right, slow down is negative
+			if (velocity > 0.0f)
+			{
+				slow = -Slowdown;
+			}
+			velocity += slow;
 
-                // If velocity has become slow enough or changed sign, stop completely
-                bool changed = sign != GetSign(velocity);
-                if (Mathf.Abs(velocity) < StopTreshold || changed)
-                {
-                    velocity = 0.0f;
-                }
-            }
-            else
-            {
-                velocity += horizontal * dt;
-                // Limit velocity
-                // 1: Clamp
-                velocity = Mathf.Clamp(velocity, -MaxVelocity, MaxVelocity);
+			// If velocity has become slow enough or changed sign, stop completely
+			bool changed = sign != GetSign(velocity);
+			if (Mathf.Abs(velocity) < StopTreshold || changed)
+			{
+				velocity = 0.0f;
+			}
+		}
+		else
+		{
+			velocity += horizontal * Acceleration;
+			// Limit velocity
+			// 1: Clamp
+			velocity = Mathf.Clamp(velocity, -MaxVelocity, MaxVelocity);
 
-                // 2 Clamp manuaalisesti
-                if (velocity < -MaxVelocity)
-                {
-                    velocity = -MaxVelocity;
-                }
-                else if (velocity > MaxVelocity)
-                {
-                    velocity = MaxVelocity;
-                }
-            }
+			// 2 Clamp manuaalisesti
+			if (velocity < -MaxVelocity)
+			{
+				velocity = -MaxVelocity;
+			}
+			else if (velocity > MaxVelocity)
+			{
+				velocity = MaxVelocity;
+			}
+		}
 
-            Vector2 pos = rb.position; // get current position
-            Vector2 newPos = pos += new Vector2(velocity, 0.0f) * dt;
+		Vector2 pos = rb.position; // get current position
+		Vector2 newPos = pos += new Vector2(velocity, 0.0f) * Time.deltaTime;
 
-            // Kinematic physics pit‰‰ laskea kiihtyvyys ja liikevoima itse
-            rb.MovePosition(newPos);
-        }
-        else if (rb.bodyType == RigidbodyType2D.Dynamic)
-        {
-            float horizontal = Input.GetAxis("Horizontal");
-            // Dynamic physics hoitaa kiihtyvyyden ja velocityn
-            // automaattisesti
-            if (horizontal != 0.0f)
-            {
-                rb.AddForce(new Vector2(horizontal * DynamicMultiplier * dt, 0.0f)); // Dynamic
-            }
-            else
-            {
-                // Add force to opposite direction until reaches StopTreshold
-                if (Mathf.Abs(rb.velocity.x) > StopTreshold)
-                {
-					float sign = GetSign(rb.velocity.x);
-                    rb.AddForce(new Vector2(-sign * DynamicSlowDown * dt, 0.0f)); // Dynamic
-                }
-                else
-                {
-                    // Stop
-                    rb.velocity = new Vector2(0.0f, 0.0f);    
-                }
-            }
-            // limit max velocity
-            if (rb.velocity.magnitude > MaxVelocity)
-            {
-                rb.velocity = rb.velocity.normalized * MaxVelocity;
-            }
-        }
-    }
+		// Kinematic physics pit‰‰ laskea kiihtyvyys ja liikevoima itse
+		rb.MovePosition(newPos);
+	}
 }
+
